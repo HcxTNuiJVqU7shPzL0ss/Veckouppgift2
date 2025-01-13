05d31f8c5cba19c59f3f7f8f1c51a0c975621342
Jan Berglund <jan.berglund@student.nbi-handelsakademin.se>
2025-01-13 17:44:31 +0100

    Refactoring
    Added checks for input values
    Changed language to English
    Added some comments
    Added more info as printouts

--- discuss_1.py
+++ discuss_1.py
@@ -1,23 +1,80 @@
-is_member = False
-level1 = 100
-level2 = 300
-discount = 0
+#####################################################################
+# Jan Berglund - discuss_1.py
+#
+# Veckouppgift 2
+# Vecka 3, 13/1
+# 1 Diskutera i grupp
+#####################################################################
 
-price = input("Välkommen, köp något dyrt: ")
-member_check = int(input("Om du är medlem, skriv 1: "))
-if member_check == 1:
+
+# Initial default variable declarations
+is_member = False   # Used to determine if member or not
+level1 = 100        # First level for discount
+level2 = 300        # Second level for discount
+discount = 0        # Discount, in percentage
+
+
+# Ask for and save a number in a variable, for "price"
+# Throw ValueError if incorrect input (not int or float), then try again
+# Negative numbers seen as positive
+print("\nYou will be asked to input the price as int or float")
+input("Press Enter to continue!\n")
+while True:
+    price = input("Welcome, please enter the price: ")
+    try:
+        price_i = int(price)                # Check if int
+        price_numeral = abs(price_i)        # Make sure positive
+        break
+    except ValueError:
+        try:
+            price_f = float(price)          # Check if float
+            price_numeral = abs(price_f)    # Make sure positive
+            break
+        except ValueError:                  # Input not int or float
+            print("Selected input not integer or float!")
+            print("Please try again...")
+            continue
+
+
+# Ask for and save an integer in a variable, to check if member
+# Throw ValueError if incorrect input, then try again
+# Negative numbers seen as positive
+print("\nYou will be asked if you are a member, please answer by input of int")
+input("Press Enter to continue!\n")
+while True:
+    try:
+        member_check = int(input("If you are a member, please type 1, "
+                                 "else any other integer: "))
+        member_check = abs(member_check)
+        break
+    except ValueError:
+        print("Not an integer, please try again!")
+
+
+if member_check == 1:   # 1 == member, set as True, else keep as False
     is_member = True
-price = float(price)
+
+price_numeral = float(price_numeral)    # Ensure float is used
+
+# Check if member, then check if price warrants a discount
 if is_member:
-    if price >= level2:
-        print("Grattis! Du har avancerat till nivå 2 och får 25% rabatt.")
+    if price_numeral >= level2:
+        print("\nCongratulations! You have advanced to Level 2, "
+              "and will receive 25% discount.")
         discount = discount + 25
-    elif price > level1:
-        print("Grattis! Du har avancerat till nivå 1 och får 10% rabatt.")
+    elif price_numeral > level1:
+        print("\nCongratulations! You have advanced to Level 1, "
+              "and will receive 10% discount.")
         discount = discount + 10
     else:
-        print("För billigt, ingen rabatt!")
+        print("\nSorry, the price does not warrant a discount.")
 else:
-    print("Du är inte medlem, du måste betala fullt pris!")
-final_price = price * (100 - discount) / 100
-print("Efter rabatter blir priset.... " + str(final_price))
+    print("\nYou are not a member, hence you will not receive"
+          "any discount.")
+input("Press Enter to continue!\n")
+
+final_price = price_numeral * (100 - discount) / 100
+
+print("\nYour price, including any discount, "
+      "is: " + str(final_price))
+input("Press Enter to continue!\n")
